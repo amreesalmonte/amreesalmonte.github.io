c7c255acd0a117f062d69fe413f78ed6345c3811
amreesalmonte <amreesalmonte@gmail.com>
2020-05-11 23:14:15 -0600

    Updates

---
+++ precache-manifest.66a61b96a3e962aeb0b78db4cca62994.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f77cd4c7bb1e9d2c64ff3ae75b8287f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8bc3cb464ca63281dc39",
+    "url": "/static/css/main.76eb6b70.chunk.css"
+  },
+  {
+    "revision": "317e83e4826493e289cb",
+    "url": "/static/js/2.2ab9b9b9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.2ab9b9b9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8bc3cb464ca63281dc39",
+    "url": "/static/js/main.42208de3.chunk.js"
+  },
+  {
+    "revision": "8cdb6dacaa4f1cebc8b9",
+    "url": "/static/js/runtime-main.6b983ece.js"
+  }
+]);
